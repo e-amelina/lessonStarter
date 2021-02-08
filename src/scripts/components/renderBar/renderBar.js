e5d0810244265f29c97fbd344da25ac1d70c5929
MariaChernousova <chernousova.maria.kr@gmail.com>
2021-02-08 13:07:06 +0200

    delete toLocaleDateString

--- src/scripts/components/renderBar/renderBar.js
+++ src/scripts/components/renderBar/renderBar.js
@@ -2,12 +2,16 @@ export const renderBar = ({ appElement, currentDate, pickCurrentDate}) => {
 
   const calendarToolbar = document.createElement("div");
   calendarToolbar.prepend(createButton(pickCurrentDate, false));
-  calendarToolbar.append(createDisplay(currentDate.toLocaleString(undefined, {month:'long'}), currentDate.getFullYear()));
+  calendarToolbar.append(createDisplay(currentDate.getMonth(), currentDate.getFullYear()));
   calendarToolbar.append(createButton(pickCurrentDate, true));
 
   calendarToolbar.classList.add("calendarBar");
   appElement.prepend(calendarToolbar);
 };
+
+export const monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
 
 function createButton (pickCurrentDate, increased) {
   const button = document.createElement("div");
@@ -25,7 +29,7 @@ function createButton (pickCurrentDate, increased) {
 function createDisplay(month, year) {
   const display = document.createElement("span");
   display.classList.add("display-date");
-  display.innerText = `${month} ${year}`;
+  display.innerText = `${monthNames[month]} ${year}`;
 
   return display;
 }
