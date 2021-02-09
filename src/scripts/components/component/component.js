b4e43030a44621a86a66f6839f316b52ab85b3ba
MariaChernousova <chernousova.maria.kr@gmail.com>
2021-02-09 13:30:48 +0200

    Add component

---
+++ src/scripts/components/component/component.js
@@ -0,0 +1,20 @@
+export class Component {
+    constructor(parentSelector, tagName) {
+      if (typeof parentSelector === "string") {
+        this.parent = document.querySelector(parentSelector);
+      } else {
+        this.parent = parentSelector;
+      }
+      this.component = document.createElement(tagName);
+    }
+  
+    render() {
+      this.parent.append(this.component);
+      return this.component;
+    }
+
+    addContent(content) {
+        this.component.innerHTML = content;
+      }
+  }
+  
